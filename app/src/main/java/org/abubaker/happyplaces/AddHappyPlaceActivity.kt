7690d18cae22cc61ae4440d7603bc21cf27d4ffb
abubaker <mohammad_abubaker@yahoo.com>
2021-07-17 03:37:31 +0500

    Base Setup

--- app/src/main/java/org/abubaker/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/org/abubaker/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,24 @@ package org.abubaker.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import org.abubaker.happyplaces.databinding.ActivityMainBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityAddHappyPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+
+        // Inflate Layout (XML)
+        binding = DataBindingUtil.setContentView(
+            this@AddHappyPlaceActivity,
+            R.layout.activity_add_happy_place
+        )
+
     }
+
 }
