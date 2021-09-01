50823ed07cc55197f445d06cf0048948cfb326c7
abubaker <mohammad_abubaker@yahoo.com>
2021-09-02 01:57:24 +0500

    Added Empty Activity for the Details view

--- app/src/main/java/org/abubaker/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/org/abubaker/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -2,11 +2,42 @@ package org.abubaker.happyplaces.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import org.abubaker.happyplaces.R
+import org.abubaker.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import org.abubaker.happyplaces.databinding.ActivityHappyPlaceDetailBinding
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityHappyPlaceDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_happy_place_detail)
+
+        // Inflate Layout (XML)
+        binding = DataBindingUtil.setContentView(
+            this@HappyPlaceDetailActivity,
+            R.layout.activity_add_happy_place
+        )
+
+        // Enabling Support for the Toolbar
+        setSupportActionBar(binding.toolbarHappyPlaceDetail)
+
+        // Activating the Toolbar
+        val actionbar = supportActionBar
+
+        //
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // Navigate the main activity on clicking the back button inside the action bar.
+        binding.toolbarHappyPlaceDetail.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        // setContentView(R.layout.activity_happy_place_detail)
     }
+
 }
