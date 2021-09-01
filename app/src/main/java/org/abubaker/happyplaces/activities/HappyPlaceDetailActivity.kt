42ddb5bab7e9db71f42f687d1bbb9d5c4b49eba2
abubaker <mohammad_abubaker@yahoo.com>
2021-09-02 01:51:08 +0500

    Dynamically updating the UI on saving new records.

---
+++ app/src/main/java/org/abubaker/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,12 @@
+package org.abubaker.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.abubaker.happyplaces.R
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+}
