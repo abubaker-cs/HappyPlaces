52fd6fecc13063f94b7f327b7adf97edad4c3bc5
abubaker <mohammad_abubaker@yahoo.com>
2021-09-07 14:32:36 +0500

    Working...

---
+++ app/src/main/java/org/abubaker/happyplaces/activities/MapActivity.kt
@@ -0,0 +1,14 @@
+package org.abubaker.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.abubaker.happyplaces.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+
+}
