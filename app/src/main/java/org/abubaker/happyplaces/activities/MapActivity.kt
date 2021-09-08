6e3f150d820b157a65a155aa6af2f547d4df7be2
abubaker <mohammad_abubaker@yahoo.com>
2021-09-08 15:02:10 +0500

    Working on MapActivity.kt

--- app/src/main/java/org/abubaker/happyplaces/activities/MapActivity.kt
+++ app/src/main/java/org/abubaker/happyplaces/activities/MapActivity.kt
@@ -4,6 +4,11 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.abubaker.happyplaces.R
 import org.abubaker.happyplaces.databinding.ActivityHappyPlaceDetailBinding
 import org.abubaker.happyplaces.databinding.ActivityMapBinding
@@ -27,21 +32,6 @@ class MapActivity : AppCompatActivity() {
             R.layout.activity_map
         )
 
-        // Enabling Support for the Toolbar
-        setSupportActionBar(binding.toolbarMap)
-
-        // Activating the Toolbar
-        val actionbar = supportActionBar
-
-        //
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true)
-        }
-
-        // Navigate the main activity on clicking the back button inside the action bar.
-        binding.toolbarMap.setNavigationOnClickListener {
-            onBackPressed()
-        }
 
         // Receives the details through intent and used further.
         // START
@@ -50,7 +40,58 @@ class MapActivity : AppCompatActivity() {
                 intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
         }
 
+        if (mHappyPlaceDetails != null) {
 
+            // Enabling Support for the Toolbar
+            setSupportActionBar(binding.toolbarMap)
+
+            // Activating the Toolbar
+            val actionbar = supportActionBar
+
+            //
+            if (actionbar != null) {
+                actionbar.setDisplayHomeAsUpEnabled(true)
+            }
+
+            // Navigate the main activity on clicking the back button inside the action bar.
+            binding.toolbarMap.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            //
+            val supportMapFragment: SupportMapFragment =
+                supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+            //
+            supportMapFragment.getMapAsync(this)
+        }
+
+
+    }
+
+    // TODO (Step 4: After extending an interface adding the location pin to the map when the map is ready using the latitude and longitude.)
+    // START
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        /**
+         * Add a marker on the location using the latitude and longitude and move the camera to it.
+         */
+        val position = LatLng(
+            mHappyPlaceDetails!!.latitude,
+            mHappyPlaceDetails!!.longitude
+        )
+        googleMap.addMarker(MarkerOptions().position(position).title(mHappyPlaceDetails!!.location))
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 15f)
+        googleMap.animateCamera(newLatLngZoom)
     }
 
 }
